d4820550ce8fb0e16764e6f354aa29f248fc3676
Kiino007 <kino007kevin@gmail.com>
2025-01-17 18:43:39 -0500

    Primer ejercicio de la clase
    Primer ejercicio desarrollado en clase

---
+++ Clase_2.py
@@ -0,0 +1,31 @@
+#Comprender como se manejan los arreglos de python
+#Realizar operaciones basicas u añlgunas avanzadas para resolver problemas de la vida cotidiana
+#Aplicas logica d eprogramacion para manipular datos almacenados en arreglos
+
+#Conceptos fundamientales del tema de los arreglos
+
+# 1. Validar que es un arreglo
+#Es una lista o una forma de estructura de datos que almacena una secuencia de elementos de forma consecutiva
+#Cada elemento se puede encontrar por medio de su indice 
+
+# 2. Uso comun de los vectores/Arreglos/lista/Array
+#Guarda datos relacionados como nombres numeros o resultados
+#Tambien sirve para procesar grandes cantidades de informacion, esto de forma ordenada
+
+# 3. Operaciones basicas
+#Crear y llenar
+#Recorrer y manipular datos
+#Realizar busquedas, filtros y transformaciones 
+
+#Ejemplos detallados
+#Crear y mostrr un vector
+
+vector = []
+
+#Vamos a llenar el vector por valores definidos por el usuario
+
+for i in range(5):
+    valor = int(input(f"Ingrese un numero para la posicion {i}:"))
+    vector.append(valor) #.Append es una funcion de python que recibe como parametro lo que se va agregar a un arreglo 
+    #y lo coloca en la ultima posicion disponible
+print(f"Arreglo ingresado: {vector}")
